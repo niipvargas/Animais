192c6bfc6e2b56aa1fca07e065ca998fde5b445b
Iani P. de Vargas <82095629+niipvargas@users.noreply.github.com>
2021-05-28 09:39:45 -0300

    Update Cachorro.java

--- src/atividade2/Cachorro.java
+++ src/atividade2/Cachorro.java
@@ -10,7 +10,8 @@ public class Cachorro extends Animal {
     @Override
     public void emitirSom() {
         System.out.println("Nome é: " + this.Nome);
-        System.out.println("Esse animal late e tem " + this.Idade + " anos");
+        System.out.println("Idade é: "+ this.Idade+" anos");
+        System.out.println("Esse animal late");
     }
     
 }
